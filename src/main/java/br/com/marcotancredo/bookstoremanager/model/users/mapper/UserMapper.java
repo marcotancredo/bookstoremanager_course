f993e5f24382422c91af51a6c2e85fa82edce5d5
Marco Antonio Tancredo <marcotancredo@gmail.com>
2021-03-18 01:29:29 -0300

    Developed users module base classes

---
+++ src/main/java/br/com/marcotancredo/bookstoremanager/model/users/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package br.com.marcotancredo.bookstoremanager.model.users.mapper;
+
+import br.com.marcotancredo.bookstoremanager.model.users.dto.UserDTO;
+import br.com.marcotancredo.bookstoremanager.model.users.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User toModel(UserDTO userDTO);
+
+    UserDTO toDTO(User user);
+}
